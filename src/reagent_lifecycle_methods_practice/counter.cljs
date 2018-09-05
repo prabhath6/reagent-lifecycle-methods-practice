5641e88b9722e0e599a7c18471e702113559e7e5
prabhath6 <prabhathkiran20@gmail.com>
2018-09-04 22:37:02 -0500

    Update the counter component to include all the lifecycle methods in it

--- src/reagent_lifecycle_methods_practice/counter.cljs
+++ src/reagent_lifecycle_methods_practice/counter.cljs
@@ -1,7 +1,24 @@
 (ns reagent-lifecycle-methods-practice.counter
-  (:require [reagent-lifecycle-methods-practice.state :as st]))
+  (:require [reagent-lifecycle-methods-practice.state :as st]
+            [reagent.core :as r]))
 
 (defn counter []
-  [:div
-   [:h1.text-muted "Counter: " @st/click-count]])
+  (r/create-class
+   {:component-did-mount
+    (fn []
+      (prn "Component Did mount"))
+
+    :component-did-update
+    (fn []
+      (prn "Component Did update"))
+
+    :component-will-unmount
+    (fn []
+      (prn "Component Did un-mount"))
+
+    :reagent-render
+    (fn []
+      (prn "Component rendered")
+      [:div
+      [:h1.text-muted "Counter: " @st/click-count]])}))
 
